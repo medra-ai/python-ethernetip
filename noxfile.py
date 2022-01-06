74c7edfc72ed43658f3fbc877b24373b776455a8
Sebastian Block <github@world-wi.de>
2022-01-06 18:10:18 +0100

    use nox for lint and calling pytest / coverage reporting

---
+++ noxfile.py
@@ -0,0 +1,29 @@
+import nox
+
+@nox.session()
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", "src/ethernetip/ethernetip.py")
+
+@nox.session(python=["3.7","3.9"])
+def tests(session):
+    session.install(".")
+    session.install("pytest", "pytest-cov")
+    tests = session.posargs or ["tests/"]
+    session.run("pytest",
+            "--cov=ethernetip",
+            "--cov-report=html:coverage-reports/" + session.python,
+            *tests,
+            env={"COVERAGE_FILE": f".coverage.{session.python}"},
+            )
+    session.notify("coverage")
+
+@nox.session
+def coverage(session):
+    session.install("coverage[toml]")
+    try:
+        session.run("coverage", "combine")
+    except:
+        pass
+    session.run("coverage", "report")
+
